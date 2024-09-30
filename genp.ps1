68f6d431c2a5eed44db4b493491addfb55fce028
bttqjs <167028866+bttqjs@users.noreply.github.com>
2024-09-30 14:10:11 +0200

    Create genp.ps1

---
+++ genp.ps1
@@ -0,0 +1,31 @@
+param (
+    [int]$action  #0 - Disable, 1 - Enable
+)
+
+function Set-FirewallRules {
+    param (
+        [string]$ruleName,
+        [bool]$enable
+    )
+    
+    $rules = Get-NetFirewallRule | Where-Object { $_.DisplayName -like "*adobe-block*" }
+    
+    foreach ($rule in $rules) {
+        if ($enable) {
+            # Enable rule
+            Enable-NetFirewallRule -Name $rule.Name
+            Write-Host "Włączono regułę: $($rule.DisplayName)"
+        } else {
+            # Disable rule
+            Disable-NetFirewallRule -Name $rule.Name
+            Write-Host "Wyłączono regułę: $($rule.DisplayName)"
+        }
+    }
+}
+if ($action -eq 0) {
+    Set-FirewallRules -ruleName "adobe-block" -enable $false
+} elseif ($action -eq 1) {
+    Set-FirewallRules -ruleName "adobe-block" -enable $true
+} else {
+    Write-Host "Incorrect value, use 1 - for Enable, 0 - for Disable"
+}
